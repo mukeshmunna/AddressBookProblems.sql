ee9c5df564ba91d67485fabe0095843bc8771b42
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-09-01 17:44:46 +0530

    [Mukesh] Add : Database creation of Address Book

--- AddressBookSqlQuery/AddressBookOperation.cs
+++ AddressBookSqlQuery/AddressBookOperation.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,14 +9,8 @@ namespace AddressBookSystem_ADO
 {
     public class AddressBookOperation
     {
-        private SqlConnection con;    
-        private void connection()
-        {
-            string connectionStr = "data source = (localdb)\\MSSQLLocalDB; initial catalog=AddressBookService;integrated security = true ";
-            con = new SqlConnection(connectionStr);
-        }
 
-        public void CreateDatabase()
+        public void DatabaseCeated()
         {
             SqlConnection con = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog=master;integrated security = true ");
             try
@@ -37,76 +30,5 @@ namespace AddressBookSystem_ADO
                 con.Close();
             }
         }
-        public void CreateTable()
-        {
-            try
-            {
-                connection();
-                SqlCommand com = new SqlCommand("AddEmployee", con);
-                com.CommandType = CommandType.StoredProcedure;
-                string query = "Create table AddressBook(\r\nid int primary key identity(1,1),\r\nfirstName varchar(20),\r\nlastName varchar(20),\r\naddress varchar(30), \r\ncity varchar(20),\r\nstate varchar(20), \r\nzip bigint, \r\nphone varchar(10),\r\nemail varchar(30),\r\n)";
-                SqlCommand cmd = new SqlCommand(query, con);
-                con.Open();
-                cmd.ExecuteNonQuery();
-                Console.WriteLine("Table  Created Suucessfully");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Date Not Updated");
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-        public void exuctedorNot(bool data)
-        {
-            if (data)
-            {
-                Console.WriteLine("Code executed");
-                return;
-            }
-            Console.WriteLine("Something went wrong");
-        }
-
-        public bool AddEmployee(AddressModel obj)
-        {
-            try
-            {
-                connection();
-                SqlCommand com = new SqlCommand("AddContactDetails", con);
-                com.CommandType = CommandType.StoredProcedure;
-                com.Parameters.AddWithValue("@FirstName", obj.FirstName);
-                com.Parameters.AddWithValue("@LastName", obj.LastName);
-                com.Parameters.AddWithValue("@Address", obj.Address);
-                com.Parameters.AddWithValue("@City", obj.City);
-                com.Parameters.AddWithValue("@State", obj.State);
-                com.Parameters.AddWithValue("@Zip", obj.Zip);
-                com.Parameters.AddWithValue("@PhoneNumber", obj.PhoneNumber);
-                com.Parameters.AddWithValue("@Email", obj.Email);
-                con.Open();
-                int i = com.ExecuteNonQuery();
-                con.Close();
-                if (i != 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-        }
-
     }
 }
--- AddressBookSqlQuery/Program.cs
+++ AddressBookSqlQuery/Program.cs
@@ -4,20 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(" AddressBookSystem ");
+        Console.WriteLine("AddressBookSystem");
         AddressBookOperation Operation = new AddressBookOperation();
-        AddressModel data = new AddressModel()
-        {
-            FirstName = "Mukesh",
-            LastName = "Mukkara",
-            Address = "sullurpeta",
-            City = "thirupathi",
-            State = "Andhrapradhesh",
-            Zip = 524121,
-            PhoneNumber = "8765432190",
-            Email = "abcd@gmail.com",
-
-        };
-        Operation.exuctedorNot(Operation.AddEmployee(data));
+        Operation.DatabaseCeated();
     }
 }
